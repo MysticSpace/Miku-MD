1c4f6e4e3675a0306523ab833934c8cad127b687
MysticSpace <113615611+MysticSpace@users.noreply.github.com>
2022-09-15 22:29:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['123']
+global.OwnerNumber = ['123']
+global.ownertag = ['123']
+global.BotName = "Levi"
+global.packname = "Levi bot"
+global.author = "By: Light"
+global.OwnerName = "Light"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
